7803261625ee26ce43bc7e4e9fd7c4c11044312f
Uriel Tapia Avalos <uriel.tapia@inai.org.mx>
2022-01-17 22:44:10 -0600

    se agrega dependecia valdiaciones, y se agregan valdiaciones en controller

--- src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
+++ src/main/java/com/formacionbdi/microservicios/commons/controllers/CommonController.java
@@ -1,10 +1,15 @@
 package com.formacionbdi.microservicios.commons.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,49 +18,58 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.formacionbdi.microservicios.commons.services.ICommonService;
 
-
-
 /**
-* @author: URTAAV
-*/
+ * @author: URTAAV
+ */
 
-
-public class CommonController<E,S extends ICommonService<E>> {
+public class CommonController<E, S extends ICommonService<E>> {
 
 	@Autowired
 	protected S service;
-	
-	 // List All 
-    @GetMapping("/")
-	public ResponseEntity<?> listar(){
-		return  ResponseEntity.ok().body(service.findAll());
+
+	// List All
+	@GetMapping("/")
+	public ResponseEntity<?> listar() {
+		return ResponseEntity.ok().body(service.findAll());
 	}
-    // List One 
-    @GetMapping("/{id}")
-	public ResponseEntity<?> ver(@PathVariable Long id){
-		
+
+	// List One
+	@GetMapping("/{id}")
+	public ResponseEntity<?> ver(@PathVariable Long id) {
+
 		Optional<E> o = service.findById(id);
-		
-		if(o == null) {
+
+		if (o == null) {
 			return ResponseEntity.notFound().build();
 		}
-		return  ResponseEntity.ok(o.get());
+		return ResponseEntity.ok(o.get());
 	}
-	
- // Create New 
-    @PostMapping
-	public ResponseEntity<?> crear(@RequestBody E entity){
+
+	// Create New
+	@PostMapping
+	public ResponseEntity<?> crear(@Valid @RequestBody E entity, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
 		E entityDB = service.save(entity);
 		return ResponseEntity.status(HttpStatus.CREATED).body(entityDB);
 	}
 
-	
- // Delete 
+	// Delete
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> eliminar(@PathVariable Long id){
-    	service.deleteById(id);
+	public ResponseEntity<?> eliminar(@PathVariable Long id) {
+		service.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	
+
+	protected ResponseEntity<?> validar(BindingResult result) {
+		Map<String, Object> errores = new HashMap<>();
+		result.getFieldErrors().forEach(err -> {
+			errores.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+		});
+
+		return ResponseEntity.badRequest().body(errores);
+	}
+
 }
